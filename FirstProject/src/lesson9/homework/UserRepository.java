092f3aa62ce8a338401f8d52b16282b7c4ce0910
Zhenya Shepel <shepelzz@gmail.com>
2018-06-06 10:20:25 +0300

    пп

--- FirstProject/src/lesson9/homework/UserRepository.java
+++ FirstProject/src/lesson9/homework/UserRepository.java
@@ -28,15 +28,10 @@ public class UserRepository {
         }
 
         long[] idsList = new long[idxCount];
-        for(int i = 0; i<idsList.length; i++){
-            idsList[i] = -1;    //так как примитивный массив инициализируется с нулями внутри
-                                //заполняю его значениями -1
-                                //потому что если будут нули и у юзера вдруг id == 0, то не получится ничего(
-        }
 
         for(User user : users){
             for(int i = 0; i < idsList.length; i++){
-                if(idsList[i] == -1 && user != null){
+                if(idsList[i] == 0 && user != null){
                     idsList[i] = user.getId();
                     break;
                 }
@@ -54,4 +49,92 @@ public class UserRepository {
         }
         return userName;
     }
+
+    public User getUserByName(String name){
+        User result = null;
+
+        for(User user : users){
+            if( user != null && user.getName() == name){
+                result = user;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public User getUserById(long id){
+        User result = null;
+
+        for(User user : users){
+            if( user != null && user.getId() == id){
+                result = user;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public User getUserBySessionId(String session){
+        User result = null;
+
+        for(User user : users){
+            if( user != null && user.getSessionId() == session){
+                result = user;
+                break;
+            }
+        }
+        return result;
+    }
+
+    private User findById(long id){
+        User result = null;
+
+        for(User user : users){
+            if( user != null && user.getId() == id){
+                result = user;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public User save(User user){
+        User result = null;
+
+        if(findById(user.getId()) == null){
+            for(int i = 0; i < users.length; i++){
+                if(users[i] == null) {
+                    users[i] = user;
+                    result = user;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public User update(User user){
+        User result = null;
+
+        for(int i = 0; i < users.length; i++){
+            if(findById(user.getId()) != null && users[i].getId() == findById(user.getId()).getId()){
+                users[i] = user;
+                result = user;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public void delete(long id){
+        for(int i = 0; i < users.length; i++){
+            if(users[i] != null && users[i].getId() == id){
+                users[i] = null;
+                break;
+            }
+        }
+    }
+
 }
