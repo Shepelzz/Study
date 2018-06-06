ba36bdb27079e4a6a80fe81aa2a427cdaddd1917
Zhenya Shepel <shepelzz@gmail.com>
2018-06-06 10:37:49 +0300

    пп

---
+++ FirstProject/src/lesson10/abstractexample/MySQLProvider.java
@@ -0,0 +1,20 @@
+package lesson10.abstractexample;
+
+public class MySQLProvider extends DbProvider{
+
+    public MySQLProvider(String dbHost) {
+        super(dbHost);
+    }
+
+    @Override
+    void connectToDb(){
+        //
+    }
+
+    @Override
+    void disconnectFromDb() {
+
+    }
+
+
+}
