091d522bc598f1cc7203ffb038317ddcece39f9b
Zhenya Shepel <shepelzz@gmail.com>
2018-06-08 17:56:27 +0300

    пп

--- FirstProject/src/lesson12/Main.java
+++ FirstProject/src/lesson12/Main.java
@@ -6,7 +6,7 @@ public class Main {
         Bank euBank = new EUBank(1373, "Sweden", Currency.EUR, 120, 1800, 5,84400);
         Bank chinaBank = new ChinaBank(1004, "China", Currency.USD, 1500, 500, 34,94008008);
 
-        User user1 = new User(1001, "Denis", 12200, 40, "GMD", 1750, usBank);
+        User user1 = new User(1001, "Denis", 1000, 40, "GMD", 1750, usBank);
         User user2 = new User(1002, "Anton", 500, 3, "GMD", 600, usBank);
         User user3 = new User(1003, "Liza", 50877, 84, "GMD", 3220, euBank);
         User user4 = new User(1004, "Matt", 120, 1, "GMD", 1100, euBank);
@@ -17,21 +17,22 @@ public class Main {
 
         BankSystem bankSystem = new UkrainianBankSystem();
 
-        /*System.out.println(user1.toString());
-        System.out.println(user5.toString());
+        System.out.println(user1.toString());
+        System.out.println(user2.toString());
 
-        bankSystem.transferMoney(user1, user5, 300);
+        bankSystem.transferMoney(user1, user2, 0);
+        System.out.println();
 
         System.out.println(user1.toString());
-        System.out.println(user5.toString());*/
+        System.out.println(user2.toString());
 
-        for(User user : users){
+        /*for(User user : users){
             bankSystem.withdraw(user, 150);
             bankSystem.fund(user, 59);
             bankSystem.transferMoney(user, user2, 43);
             bankSystem.paySalary(user);
 
             System.out.println(user.getBalance());
-        }
+        }*/
     }
 }
